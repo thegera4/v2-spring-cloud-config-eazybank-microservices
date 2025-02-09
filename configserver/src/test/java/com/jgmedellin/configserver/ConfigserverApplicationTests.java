e49e03a896ce5af275caced883fa0f7d9015fbb8
Gerardo Medellin <out_jmedellin@findep.com.mx>
2025-02-09 16:25:36 -0600

    update: 1st approach spring cloud server config classpath

---
+++ configserver/src/test/java/com/jgmedellin/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgmedellin.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
